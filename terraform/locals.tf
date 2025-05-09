8b2bbf02e79e42e97bf567cf08181218acd902cd
jbrcoleman <55991476+jbrcoleman@users.noreply.github.com>
2025-05-09 16:29:56 +0000

    add terraform config for crossplane

---
+++ terraform/locals.tf
@@ -0,0 +1,16 @@
+locals {
+  name   = var.name
+  region = var.region
+
+  cluster_version = var.cluster_version
+  cluster_name    = local.name
+
+  vpc_name = local.name
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Blueprint  = local.name
+    GithubRepo = "github.com/awslabs/crossplane-on-eks"
+  }
+}
